ec241b49f0993134e79514aeed300dc7ead45b0a
porschelook <33505600+porschelook@users.noreply.github.com>
2025-07-03 00:29:15 +0700

    wordle

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [goal, setGoal] = useState("apple");
+  const [types, setTypes] = useState<string[][]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [len, setLen] = useState<number>(0);
+
+  useEffect(() => {
+    // setGoal() random word from a list of words
+    // fetch data from an API or use a static list
+  }, []);
+
+  useEffect(() => {
+    if (len === 6) {
+      alert("END GAME");
+      return;
+    }
+  }, [len]);
+
+  const handleEnter = () => {
+    if (inputValue.length !== 5) {
+      alert("Please enter a 5-letter word.");
+      return;
+    }
+
+    setTypes([...types, inputValue.split("")]);
+    setLen(types.length + 1);
+    setInputValue(""); // Clear input after processing
+
+    if (inputValue === goal) {
+      alert("You guessed the word!");
+      return;
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>{goal}</p>
+        <div>
+          {types.map((guess, idx) => (
+            <BoxLine key={idx} guess={guess} goal={goal} />
+          ))}
+        </div>
+        <input
+          type="text"
+          value={inputValue}
+          maxLength={5}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button onClick={handleEnter}>Enter</button>
+        <p>{len}</p>
       </header>
+    </div>
+  );
+}
+
+function BoxLine({ guess, goal }: { guess: string[]; goal: string }) {
+  return (
+    <div className="box-line">
+      {Array.from({ length: 5 }).map((_, index) => {
+        let color = "#ccc";
+        if (guess[index]) {
+          if (guess[index] === goal[index]) {
+            color = "#6aaa64"; // green
+          } else if (goal.includes(guess[index])) {
+            color = "#c9b458"; // yellow
+          } else {
+            color = "#ff5656"; // red
+          }
+        }
+
+        return (
+          <span
+            key={index}
+            className="box"
+            style={{ backgroundColor: color }}
+          >
+            {guess[index] ? guess[index] : "?"}{" "}
+          </span>
+        );
+      })}
     </div>
   );
 }
